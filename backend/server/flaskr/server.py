f5f49b11e7e64214e25c5505816bede1681bbb4a
Sparsh <sarodesparsh@gmail.com>
2024-10-14 18:28:41 -0400

    get flask to run on local network

--- backend/server/flaskr/server.py
+++ backend/server/flaskr/server.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/delete-preference", methods=['POST'])
+def delete_preference():
+    data = {'message': 'preferences deleted'}
+    response = make_response(jsonify(data), 200)
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
