7dd43be37f2147648bfb6aab792ad47f3fd1cda9
Sparsh <sarodesparsh@gmail.com>
2024-10-14 18:29:02 -0400

    connect app to server

--- app/App.tsx
+++ app/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,6 +15,9 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  ActivityIndicator,
+  ToastAndroid 
 } from 'react-native';
 
 import {
@@ -55,63 +58,157 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   );
 }
 
+// function App(): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isFetching, setIsFetching] = useState(false);
+  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = false;
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
+    //   <ScrollView>
+        
+    //   </ScrollView>
+    // </SafeAreaView>
+    <View style={styles.buttonContainer}>
+      <Button 
+        title="Delete Preferences"
+        onPress={() => {deletePreferences(setIsFetching)}}
+        disabled={isFetching}
+      >
+        
+        
+      </Button>
+      
+      {isFetching && <View>
+        <ActivityIndicator
+          size="small"
+          color="white"
+          style={{ marginRight: 10 }}
+        />
+        <Text style={styles.buttonText}>Deleting...</Text>
+        </View>}
+    </View>
   );
 }
 
+async function deletePreferences(setIsFetching: React.Dispatch<React.SetStateAction<boolean>>) {
+  setIsFetching(true);
+  console.log('making api call');
+  // setTimeout(() => {setIsFetching(false)}, 1000)
+
+  try {
+    const response = await fetch(
+      'http://192.168.1.167:5000/api/delete-preference',
+      {
+        method: 'POST'
+      }
+    );
+    const status = response.status;
+    if(status != 200) {
+      console.log('failed to fetch')
+      ToastAndroid.show('Failed to delete your preference', ToastAndroid.SHORT);
+    } else {
+      console.log('successfully deleted')
+      ToastAndroid.show('Successfully deleted your preferences', ToastAndroid.SHORT);
+    }
+    setIsFetching(false);
+  } catch (error) {
+    console.error(error);
+    setIsFetching(false);
+  }
+  // setIsFetching(false);
+}
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   }
+// });
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  buttonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button:   
+ {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
