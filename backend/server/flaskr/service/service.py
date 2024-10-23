8375a5937ef8ddc03a6e3f9583caea00e6303394
Sparsh <sarodesparsh@gmail.com>
2024-10-23 18:47:00 -0400

    move tests to "tests/" dir

--- backend/server/flaskr/service/service.py
+++ backend/server/flaskr/service/service.py
@@ -1,9 +1,8 @@
 import os
 def delete_preferences(req):
     # TODO: implement this once language model design is finalized
-    if os.path.exists(req['filepath']):
-        try:
-            os.remove(req['filepath'])
-        except OSError as e:
-            return False, str(e)
+    try:
+        os.remove(req['filepath'])
+    except OSError as e:
+        return False, str(e)
     return True, ""
--- backend/server/tests/test_service.py
+++ backend/server/tests/test_service.py
@@ -1,8 +1,8 @@
 from unittest.mock import patch, Mock
-from service import delete_preferences
+from flaskr.service.service import delete_preferences
 import pytest
+import os
 
-# @patch('os.remove', Mock())
 def test_delete_preferences_raises_error():
     mocked_error = 'insufficient file permission'
     with patch('os.remove', side_effect=OSError(mocked_error)):
@@ -16,4 +16,4 @@ def test_delete_preferences_deletes_file(mocker):
     done, err = delete_preferences({'filepath': 'dummy/file/path'})
     mock_os_remove.assert_called()
     assert done == True
-    assert err ==""
+    assert err == ""
